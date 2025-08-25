1c8db663700365a01565da748dc55fbc9fb50b0c
Renan-Marquet <224832176+Renan-Marquet@users.noreply.github.com>
2025-08-25 15:35:54 -0300

    acertei de inteiro para string

--- aula07_20250825/exercicio_03.py
+++ aula07_20250825/exercicio_03.py
@@ -7,14 +7,14 @@ print(f"Para saber a posição do robô digite 3")
 print(f"Para desligar digite 4")
 while True:
     
-    commando=int(input("digite seu comando -> "))
-    if commando == 1:
+    commando=input("digite seu comando -> ")
+    if commando == "1":
         posicao_atual+=1
-    elif commando == 2:
+    elif commando == "2":
         posicao_atual-=1
-    elif commando == 3:
+    elif commando == "3":
         print(f"A posição atual é :   -->{posicao_atual}")
-    elif commando == 4:
+    elif commando == "4":
         print(f"O Robô foi desligado na posição :{posicao_atual}")
         break
     else:
