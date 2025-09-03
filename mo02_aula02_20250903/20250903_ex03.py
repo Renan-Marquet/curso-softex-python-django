62e7f23aa73300136637f0d35e79ae577ecc9877
Renan-Marquet <224832176+Renan-Marquet@users.noreply.github.com>
2025-09-03 15:34:31 -0300

    exercicio3

---
+++ mo02_aula02_20250903/20250903_ex03.py
@@ -0,0 +1,16 @@
+acessos = [("Pedro","sucesso"),("Ana","falha"),("Maria","sucesso"),("Pedro","falha"),("Ana","falha")]
+sucesso=set()
+falha=set()
+so_falha=set()
+
+for nome,status in acessos:
+    if status == "sucesso" :
+        sucesso.add(nome)
+    if status == "falha" :
+        falha.add(nome)
+
+so_falha=falha.difference(sucesso)
+       
+print(falha)
+print(sucesso)
+print(so_falha)
