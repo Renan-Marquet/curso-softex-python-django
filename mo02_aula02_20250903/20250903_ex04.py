5d11b2fbd1c3bf9cfc810946a35c2a9e3a47cab8
Renan-Marquet <224832176+Renan-Marquet@users.noreply.github.com>
2025-09-03 15:53:47 -0300

    exercicio 4

---
+++ mo02_aula02_20250903/20250903_ex04.py
@@ -0,0 +1,19 @@
+notas=[("Ana",9.5),("João",8.0),("Maria",10.0),("Pedro",7.5),("Ana",10.0),("Carlos",6.5)]
+reprovados=set()
+melhores=set()
+maior=0
+
+for nome,nota in notas:
+    if nota < 7.0 :
+        reprovados.add(nome)
+    if nota > maior:
+        maior=nota
+for nome,nota in notas:
+    if nota == maior:
+        melhores.add(nome)
+
+print("\n Maior nota -> ",maior)
+print(" Alunos com as melhores notas")
+print(melhores)
+print(" Reprovados com notas menores que 7: ")
+print(reprovados,"\n")
