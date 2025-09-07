2af0a5130af6b0b47be1141f9185d5142e1bbbef
Renan-Marquet <224832176+Renan-Marquet@users.noreply.github.com>
2025-09-07 16:01:20 -0300

    desafio feito

--- modulo_02/home_202509096/analisededados.py
+++ modulo_02/home_202509096/analisededados.py
@@ -1,24 +1,70 @@
 # Análise de Dados de Acessos
 entrada=[]
-#status=""
-teste=True
-#while True:
-usuario=input("Nome do Usuário")
-#status=input("digite 1 para sucesso ou 2 para falha")
-#print(status)
+#saida=[]
+lista=[]
+nova=()
+#lista_valida=[]
+tempo_total=0.0
+usuarios=set()
+#listc=set()
+#vazio=()
+
 while True:
-    status=input("digite 1 para sucesso ou 2 para falha")
-    if status != "1" or status != "2":
-        status=input ("entrada invalida, digite somente 1 ou 2 tente novamente")
+  
+    usuario=input("\n Entre com o nome do Usuário ou digite parar para encerrar o programa: -> ")
+    if usuario == "parar":
+        print("\n Você encerrou o programa. \n")
+        break
+
+    status=input("Digite 1 para sucesso ou 2 para falha -> ")
+
+    while True:
+        
+        if status == "1":
+            status="sucesso"
+            break
+        elif status == "2":
+            status="falha"
+            #entrada="vazio"
+            break
+        else:
+            status=input(" Entrada inválida, digite apenas 1 ou 2 -> ")
+    while True:
+        if status=="falha":
+            break
+        try:
+            tempo=float(input(" Entre com a duração da sessão: -> "))
+        
+        except ValueError:
+            print(" Digite apenas numeros. -> ")
+            continue
+        
+        if tempo>0:
+            break
+        else:
+            print(" Digite apenas numeros positivos. -> ")
+         
+
+    if status == "sucesso":
+        entrada.append(usuario)
+        entrada.append(status)
+        entrada.append(tempo)
+        tempo_total=tempo_total+tempo
+        usuarios.add(usuario)
+
+    if entrada == ():
+        entrada=[]
+        break
+        #print(entrada)
     else:
-        False
-tempo=input("entre com a duração")
-#    except:
+        nova=tuple(entrada)
+        entrada=[]
+        #print(nova)
 
-entrada.append(usuario)
-entrada.append(status)
-entrada.append(tempo)
+        lista.append(nova)
+      
+print(" Registro dos acessos Válidos: ->  ",lista)
+print(" Usuários bem sucedidos: -> ", usuarios)
+print(" Tempo total de acesso: - > ", tempo_total)
 
-print(entrada)
-nova=tuple(entrada)
-print(nova)
+
