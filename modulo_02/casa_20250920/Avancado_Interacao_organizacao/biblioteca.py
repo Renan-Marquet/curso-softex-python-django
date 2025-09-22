45e25d3958fd1780c2e3adafbfd33064643031a7
Renan-Marquet <224832176+Renan-Marquet@users.noreply.github.com>
2025-09-21 23:28:07 -0300

    por hoje é só ainda não consegui o ex11

--- modulo_02/casa_20250920/Avancado_Interacao_organizacao/biblioteca.py
+++ modulo_02/casa_20250920/Avancado_Interacao_organizacao/biblioteca.py
@@ -1,16 +1,38 @@
+
+acervo=[]
+
 class Livro:
     def __init__(self,titulo:str,autor:str)-> None:
         self.titulo=titulo
         self.autor=autor
-        pass
-    pass
-
+       
 class Biblioteca:
     def __init__(self,acervo:list) -> None:
-        self.acervo=[]
+        self.acervo=acervo
 
-    def adicionar_livro(self):
-        pass
+    def adicionar_livro(self, livro: list) -> None:
+        self.livro=livro
+        while True:
+            self.acervo.append(livro)
+            self.livro=(titulo,autor)
+            entrada=input("Deseja adicionar um livro? Digite S para confirmar -> ")
+            entrada.lower()
+            if entrada == "s" or entrada == "S":
+                titulo=input("Entre como o nome do Livro: -> ")
+                autor=input(f"Entre com o autor de {self.titulo}: -> ")
+                self.livro=Livro(titulo,autor)
+                
+                print(self.livro)
+                self.acervo.append(self.livro)
+            else:
+                break
 
     def listar_livros(self):
-        pass
+        self.livro=livro
+        for livro in self.acervo:
+            print(livro.self.titulo,livro.self.autor)
+
+Biblioteca.adicionar_livro()
+Biblioteca.listar_livros()
+
+
