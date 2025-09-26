8d42bd0126e6073014dd3a03b38be7f8fb412a1e
Renan-Marquet <224832176+Renan-Marquet@users.noreply.github.com>
2025-09-26 15:42:50 -0300

    Update hierarquida_geometrica.py

--- modulo_02/aula07___20250924/hierarquida_geometrica.py
+++ modulo_02/aula07___20250924/hierarquida_geometrica.py
@@ -3,25 +3,30 @@ class FormaGeometrica:
         self.cor=cor
 
     def calcular_area(self) -> None:
-        a=0
+        pass
 
     def __str__(self):
-        return f"{self.cor}"
+        return f"A cor é {self.cor}"
         
 class Retangulo(FormaGeometrica):
     def __init__(self, cor, largura :float, altura :float):
         super().__init__(cor)
         self.largura=largura
         self.altura=altura
+   
     def calcular_area(self):
         arear=self.largura*self.altura
-        print(f"A área do retângulo de {self.largura} por {self.altura} é de {arear}")
-        
+        # print(f"A área do retângulo de {self.largura} por {self.altura} é de {arear}")
+        return arear
+    
 class Quadrado(Retangulo):
     def __init__(self, cor, largura, altura, lado:float):
         super().__init__(cor, largura, altura)
-        self.largura=lado
-        self.altura=lado
+        self.lado=lado
+    def calcular_area(self):
+        areaq=self.lado**2
+        #print(f"A área do quadrado de {self.ladoa} de lado é de {areaq}")
+        return areaq
 
 
 cor="branco"
@@ -33,4 +38,9 @@ retangulo=Retangulo(ob,largura,altura)
 quadrado=Quadrado(ob,lado,lado,lado)
 retquad=(retangulo,quadrado)
 
+print(retangulo)
 #def calcular_soma_areas():
+respostaderet=retangulo.calcular_area()
+print(respostaderet)
+
+print(f"A área do retângulo de cor {retangulo.cor} com {retangulo.largura} por {retangulo.altura} é de {retangulo.calcular_area()}")
