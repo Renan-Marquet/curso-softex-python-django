ac1553883f1c339925aec5af32ad7f8f53c47e13
Renan-Marquet <224832176+Renan-Marquet@users.noreply.github.com>
2025-10-15 13:45:19 -0300

    nova tentativa

--- modulo_03/aula_04_21_20251013/blog_model.py
+++ modulo_03/aula_04_21_20251013/blog_model.py
@@ -22,3 +22,109 @@ Faça um CRUD para:
 
 **Consulte o UserModel para se guiar
 """
+
+
+import sqlite3
+from datetime import datetime
+from database import DatabaseConnection
+
+
+class BlogModel:
+    """Gerencia a tabela 'blogs' todas as operações de CRUD."""
+
+    def __init__(self):
+        self.db_conn = DatabaseConnection()
+        self._create_blogs()
+
+    def _create_blogs(self):
+        """Método privado para criar a tabela de blogs."""
+        self.db_conn.connect()
+        self.db_conn.cursor.execute(
+            """
+            CREATE TABLE IF NOT EXISTS postagem (
+                id_post INTEGER PRIMARY KEY AUTOINCREMENT,
+                titulo TEXT NOT NULL UNIQUE,
+                conteudo TEXT NOT NULL,
+                data_criacao DATETIME DEFAULT CURRENT_TIMESTAMP,
+                data_atualizacao DATETIME DEFAULT CURRENT_TIMESTAMP
+                id_user INTEGER,
+                FOREIGN KEY (id_user) REFERENCES usuarios(id)
+            );
+        """
+        )
+        self.db_conn.close()
+ 
+    def create_post(self, titulo, conteudo, id_user):
+        """Cria um novo post."""
+        self.db_conn.connect()
+        try:
+            self.db_conn.cursor.execute(
+                """
+                INSERT INTO postagem (titulo, conteudo, id_user)
+                VALUES (?, ?, ?);
+            """,
+                (titulo, conteudo, id_user),
+            )
+            print("Post criado com sucesso!")
+        except sqlite3.IntegrityError:
+            print(f"Erro: O e-mail '{titulo}' já está em uso.")
+        finally:
+            self.db_conn.close()
+
+    def find_post_by_id(self, id_post):
+        """Busca um usuário pelo ID."""
+        self.db_conn.connect()
+        self.db_conn.cursor.execute("SELECT * FROM postagem WHERE id = ?;", (id_post,))
+        post = self.db_conn.cursor.fetchone()
+        self.db_conn.close()
+        return post
+       
+    def find_post_by_id_user(self, id_user):
+        """Busca um usuário pelo ID."""
+        self.db_conn.connect()
+        self.db_conn.cursor.execute("SELECT * FROM postagem WHERE id = ?;", (id_user,))
+        post = self.db_conn.cursor.fetchone()
+        self.db_conn.close()
+        return post
+
+    def update_post_by_id_post(self, id_post, titulo=None, conteudo=None):
+        """Atualiza informações de um usuário pelo ID."""
+        self.db_conn.connect()
+        updates = []
+        params = []
+        if titulo:
+            updates.append("titulo = ?")
+            params.append(titulo)
+        if conteudo:
+            updates.append("conteudo = ?")
+            params.append(conteudo)
+
+        if not updates:
+            print("Nada para atualizar.")
+            self.db_conn.close()
+            return
+
+        updates.append("data_atualizacao = ?")
+        params.append(datetime.now())
+        params.append(id_post)
+        query = f"UPDATE postagem SET {', '.join(updates)} WHERE id = ?;"
+
+        self.db_conn.cursor.execute(query, params)
+        print("Usuário atualizado com sucesso!")
+        self.db_conn.close()
+
+    def delete_post_by_id(self, id_post):
+        """Deleta uma postagem pelo ID."""
+        self.db_conn.connect()
+        self.db_conn.cursor.execute("DELETE FROM postagem WHERE id = ?;", (id_post,))
+        print("Postagem deletada com sucesso!")
+        self.db_conn.close()
+            
+    def get_all_posts(self):
+        """Retorna todas as postagens."""
+        self.db_conn.connect()
+        self.db_conn.cursor.execute("SELECT * FROM postagem;")
+        posts = self.db_conn.cursor.fetchall()
+        self.db_conn.close()
+        return posts
+
--- modulo_03/aula_04_21_20251013/main_post.py
+++ modulo_03/aula_04_21_20251013/main_post.py
@@ -13,82 +13,28 @@ def display_menu():
     print("6. Sair")
     print("---------------------------------")
     print("\n--- Gerenciador de Postagem ---")
-    print("7. Cadastrar nova Postagem")
-    print("8. Buscar Postagem por ID da postagem")
-    print("8. Buscar Postagem por ID do usuário")
-    print("A. Atualizar Postagem")
-    print("B. Deletar Postagem")
-    print("C. Listar todos as postagenss")
-    print("D. Sair")
+    print("A. Cadastrar nova Postagem")
+    print("B. Buscar Postagem por ID da postagem")
+    print("C. Buscar Postagem por ID do usuário")
+    print("D. Atualizar Postagem")
+    print("E. Deletar Postagem")
+    print("F. Listar todos as postagenss")
+    print("G. Sair")
     print("---------------------------------")
+
+
+
+
 
 def main():
     """Função principal do programa."""
-    user_model = UserModel()
     blog_model = BlogModel()
 
     while True:
         display_menu()
         choice = input("Escolha uma opção: ")
 
-        if choice == "1":
-            print("\n--- Cadastro de Usuário ---")
-            senha = input("Senha: ")
-            email = input("E-mail: ")
-            user_model.create_user(senha, email)
-
-        elif choice == "2":
-            print("\n--- Buscar Usuário ---")
-            try:
-                user_id = int(input("Digite o ID do usuário: "))
-                user = user_model.find_user_by_id(user_id)
-                if user:
-                    print("\nUsuário encontrado:")
-                    print(f"ID: {user['id']}")
-                    print(f"E-mail: {user['email']}")
-                    print(f"Data de Criação: {user['data_criacao']}")
-                else:
-                    print("Usuário não encontrado.")
-            except ValueError:
-                print("ID inválido. Por favor, digite um número.")
-
-        elif choice == "3":
-            print("\n--- Atualizar Usuário ---")
-            try:
-                user_id = int(input("Digite o ID do usuário: "))
-                print("Deixe em branco os campos que não deseja alterar.")
-                senha = input("Nova senha: ") or None
-                email = input("Novo e-mail: ") or None
-                user_model.update_user_by_id(user_id, senha, email)
-            except ValueError:
-                print("ID inválido. Por favor, digite um número.")
-
-        elif choice == "4":
-            print("\n--- Deletar Usuário ---")
-            try:
-                user_id = int(input("Digite o ID do usuário: "))
-                user_model.delete_user_by_id(user_id)
-            except ValueError:
-                print("ID inválido. Por favor, digite um número.")
-
-        elif choice == "5":
-            print("\n--- Lista de Usuários ---")
-            users = user_model.get_all_users()
-            if users:
-                for user in users:
-                    print(f"\nID: {user['id']}")
-                    print(f"E-mail: {user['email']}")
-                    print(f"Data de Criação: {user['data_criacao']}")
-                print("\n--- Fim da lista ---")
-            else:
-                print("Nenhum usuário cadastrado.")
-
-        elif choice == "6":
-            print("Saindo do programa.")
-            break
-
-
-        elif choice == "A" or "a":
+        if choice == "A" or "a":
             print("\n--- Cadastro de Postagem ---")
             titulo = input("Título: ")
             conteudo = input("Conteudo: ")
@@ -166,10 +112,8 @@ def main():
             break
 
         else:
-            print("Opção inválida. Tente novamente.")      
-       
+            print("Opção inválida. Tente novamente.")
 
 
 if __name__ == "__main__":
-    main()
-
+    main();
