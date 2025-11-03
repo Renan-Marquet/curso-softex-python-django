33cff23dac49950e20abc5e6b900eff95e1b6529
Renan-Marquet <224832176+Renan-Marquet@users.noreply.github.com>
2025-11-03 00:28:54 -0300

    tentando ex 7

--- modulo_03/50_exercicios_20251025/ex07de50_maiorseqcontigua.py
+++ modulo_03/50_exercicios_20251025/ex07de50_maiorseqcontigua.py
@@ -1,3 +1,38 @@
 # ex 7 maior sequencia contigua
 # criar funcao que receba uma lista de numeros e retorne a soma máxima de 
 # qualquer sublista contígua (algorítimo de Kadane - Avançado)
+"""
+from random import randint
+
+lista=[]
+
+for _ in range (15):
+    lista.append(randint(-9,9))
+
+print(lista)"""
+
+lista=[-5, 3, 9, 1, 8, -9, 6,-3, 4, 9, -4, 8, -2, -2, -1,]
+
+#lista=[-5, 3, 9, 1, 8, -9, 6,-3, 4, 9, -4, 8, -2, -2, -1,]
+
+def sublistaMaxima(lista):
+    #sublista=[]
+    somaMaxima=float('-inf')
+    somaAtual=0
+
+    for i in range(len(lista)):
+        somaAtual+=lista[i]
+        somaMaxima=max(somaMaxima,somaAtual)
+
+        if somaAtual<0:
+            somaAtual=0           
+            #sublista=[]
+                              
+        #else:
+            #sublista.append(lista[i]) 
+    
+    #print(sublista)    
+    return somaMaxima
+
+lista=[-2, 1, -3, 4, -1, 2, 1, -5, 4]
+print(sublistaMaxima(lista))
