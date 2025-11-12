076d7a60655cabc7942630467957b049277e723a
Renan-Marquet <224832176+Renan-Marquet@users.noreply.github.com>
2025-11-12 15:57:30 -0300

    aula 4 do modulo 4 Django

--- Django_teste1/core/views.py
+++ Django_teste1/core/views.py
@@ -1,16 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 #from django.http import HttpResponse
 from .models import Tarefa
+from .forms import TarefaForm
 
 def home(request):
     # 2. Use o ORM para buscar os dados! 
+    # 3. Lógica de POST: Se o formulário foi enviado 
+    if request.method == 'POST': 
+        # Cria uma instância do form e preenche com os dados do POST
+        form = TarefaForm(request.POST) 
+        # 4. O Django valida os dados (max_length, etc.) 
+        if form.is_valid(): 
+            # 5. Salva o objeto no banco de dados! 
+            form.save() 
+            # 6. Redireciona de volta para a 'home' 
+            # # Isso é o Padrão "Post-Redirect-Get" (PRG) 
+            return redirect('home') 
+        # Se o form NÃO for válido, o código continua e
+        # o 'form' (com os erros) será enviado para o template
+     # 7. Lógica de GET: Se o usuário apenas visitou a página
+    else: 
+        form = TarefaForm() 
+        # Cria um formulário vazio
     # # Tarefa.objects.all() significa: "Pegue todas as linhas da tabela Tarefa" 
     todas_as_tarefas = Tarefa.objects.all() 
 
     context = {
         'nome_usuario':'Renan',
         'tecnologias':['Python','Django','HTML','CSS','Models','Admin'],
-        'tarefas': todas_as_tarefas # 4. Adicione as tarefas ao contexto
+        'tarefas': todas_as_tarefas,# 4. Adicione as tarefas ao contexto
+        'form':form
     }
 
 
